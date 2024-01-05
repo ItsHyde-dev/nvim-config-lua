0edf54c516e8390603dca2c9ee899347afb853aa
Himanshu Joshi <heyitshyde@gmail.com>
2024-01-05 22:15:37 +0530

    added autotag setup

--- lua/hyde/plugins/misc_editing.lua
+++ lua/hyde/plugins/misc_editing.lua
@@ -10,5 +10,10 @@ return {
     "windwp/nvim-autopairs",
     config = function() require("nvim-autopairs").setup({}) end
   },
-  "windwp/nvim-ts-autotag",
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end
+  }
 }
