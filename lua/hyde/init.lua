c7563b2bc1e680788046ff6e4149166b139f9286
Himanshu Joshi <himanshu6.joshi@ril.com>
2024-06-21 13:20:17 +0530

    open all folds by default

--- lua/hyde/init.lua
+++ lua/hyde/init.lua
@@ -10,25 +10,34 @@ local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
 
 function R(name)
-    require("plenary.reload").reload_module(name)
+  require("plenary.reload").reload_module(name)
 end
 
 autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
+  group = yank_group,
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',
+      timeout = 40,
+    })
+  end,
 })
 
-autocmd({"BufWritePre"}, {
-    group = HydeGroup,
-    pattern = "*",
-    command = [[%s/\s\+$//e]],
+autocmd({ "BufWritePre" }, {
+  group = HydeGroup,
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
 })
+
+autocmd({ "BufWritePre" }, {
+  group = HydeGroup,
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
+})
+
+
+vim.opt.foldenable = false
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
