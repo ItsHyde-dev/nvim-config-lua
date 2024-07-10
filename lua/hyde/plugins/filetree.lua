76ae78917fd814cedef6f6ca6d81c90ec0254953
Himanshu Joshi <heyitshyde@gmail.com>
2024-07-11 02:14:29 +0530

    added oil as preferred file management

--- lua/hyde/plugins/filetree.lua
+++ lua/hyde/plugins/filetree.lua
@@ -1,21 +1,34 @@
-return {
-  "nvim-tree/nvim-tree.lua",
-  keys = {
-    { "<leader><leader>", "<cmd>:NvimTreeToggle<cr>",   desc = "open nvim tree",                 mode = "n" },
-    { "<leader>pv",       "<cmd>:NvimTreeFindFile<cr>", desc = "open nvim tree at current file", mode = "n" },
-  },
-  config = function()
-    require("nvim-tree").setup({
-      view = {
-        float = {
-          enable = true
-        }
-      }
-    })
+-- return {
+-- "nvim-tree/nvim-tree.lua",
+-- keys = {
+-- { "<leader><leader>", "<cmd>:NvimTreeToggle<cr>",   desc = "open nvim tree",                 mode = "n" },
+-- { "<leader>pv",       "<cmd>:NvimTreeFindFile<cr>", desc = "open nvim tree at current file", mode = "n" },
+-- },
+-- config = function()
+-- require("nvim-tree").setup({
+-- view = {
+-- float = {
+-- enable = true
+-- }
+-- }
+-- })
 
-    local api = require("nvim-tree.api")
-    api.events.subscribe(api.events.Event.FileCreated, function(file)
-      vim.cmd("edit " .. file.fname)
-    end)
+-- local api = require("nvim-tree.api")
+-- api.events.subscribe(api.events.Event.FileCreated, function(file)
+-- vim.cmd("edit " .. file.fname)
+-- end)
+-- end
+-- }
+
+return {
+  'stevearc/oil.nvim',
+  opts = {},
+  -- Optional dependencies
+  dependencies = { "echasnovski/mini.icons" },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  config = function()
+    require("oil").setup()
+    vim.keymap.set("n", "<leader><leader>", "<cmd>:Oil<cr>")
+    vim.keymap.set("n", "<leader>pv", "<cmd>:Oil<cr>")
   end
 }
